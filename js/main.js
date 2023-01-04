c5d571a2cd494e085598787677ec855c6657faae
Luiz <lhvalentim@outlook.coom>
2023-01-04 15:58:07 -0300

    auto update app

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-navigator.serviceWorker?.register('service-worker.js')
+navigator.serviceWorker?.register('service-worker.js').then(reg => {
+	reg.addEventListener('updatefound', () => {
+		let newWorker = reg.installing
+		newWorker?.addEventListener('statechange', () => {
+			console.log('Update Installed. Restarting...')
+			if (newWorker.state == 'activated') location.reload(true)
+		})
+	})
+})
 
 document.querySelector('#add').onclick = e => {
 	let line = document.querySelector('section:first-of-type article:first-of-type').cloneNode(true)
