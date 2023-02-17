ccf841d2b96ee5bce2dff0b46b495d43694685e2
Luiz <lhvalentim@outlook.coom>
2023-02-17 13:33:59 -0300

    fix typo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-navigator.serviceWorker?.register('service-worker.js').then(reg => {
-	reg.addEventListener('updatefound', () => {
-		let newWorker = reg.installing
-		newWorker?.addEventListener('statechange', () => {
-			console.log('Update Installed. Restarting...')
-			if (newWorker.state == 'activated') location.reload(true)
-		})
-	})
-})
+navigator.serviceWorker?.register('service-worker.js')
+navigator.serviceWorker.onmessage = m => {
+	console.info('Update found!')
+	if (m?.data == 'update') location.reload(true)
+}
 
 document.querySelector('#add').onclick = e => {
 	let line = document.querySelector('section:first-of-type article:first-of-type').cloneNode(true)
